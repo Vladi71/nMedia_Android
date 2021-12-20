6596d08d1f3ca25ae418044574d6bb0403a22007
Vladi71 <69647342+Vladi71@users.noreply.github.com>
2021-12-20 19:54:14 +0300

    ДЗ Push-уведомления

--- app/build/tmp/kapt3/stubs/debug/ru/netology/service/FCMService.java
+++ app/build/tmp/kapt3/stubs/debug/ru/netology/service/FCMService.java
@@ -2,8 +2,10 @@ package ru.netology.service;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\t\u001a\u00020\n2\u0006\u0010\u0006\u001a\u00020\u000bH\u0002J\b\u0010\f\u001a\u00020\nH\u0016J\u0010\u0010\r\u001a\u00020\n2\u0006\u0010\u000e\u001a\u00020\u000fH\u0016J\u0010\u0010\u0010\u001a\u00020\n2\u0006\u0010\u0011\u001a\u00020\u0004H\u0016J\u0010\u0010\u0012\u001a\u00020\n2\u0006\u0010\u0006\u001a\u00020\u0013H\u0003R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082D\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0004X\u0082D\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0004X\u0082D\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0014"}, d2 = {"Lru/netology/service/FCMService;", "Lcom/google/firebase/messaging/FirebaseMessagingService;", "()V", "action", "", "channelId", "content", "gson", "Lcom/google/gson/Gson;", "handleLike", "", "Lru/netology/service/Like;", "onCreate", "onMessageReceived", "message", "Lcom/google/firebase/messaging/RemoteMessage;", "onNewToken", "token", "sentPost", "Lru/netology/service/Post;", "app_debug"})
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000B\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0007\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u000f\u001a\u00020\u00102\u0006\u0010\u0011\u001a\u00020\u0012H\u0002J\u0010\u0010\u0013\u001a\u00020\u00102\u0006\u0010\f\u001a\u00020\u0014H\u0002J\u0010\u0010\u0015\u001a\u00020\u00102\u0006\u0010\f\u001a\u00020\u0016H\u0003J\b\u0010\u0017\u001a\u00020\u0010H\u0016J\u0010\u0010\u0018\u001a\u00020\u00102\u0006\u0010\u0011\u001a\u00020\u0019H\u0016J\u0010\u0010\u001a\u001a\u00020\u00102\u0006\u0010\u001b\u001a\u00020\u0004H\u0016R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082D\u00a2\u0006\u0002\n\u0000R\u001e\u0010\u0005\u001a\u00020\u00068\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0007\u0010\b\"\u0004\b\t\u0010\nR\u000e\u0010\u000b\u001a\u00020\u0004X\u0082D\u00a2\u0006\u0002\n\u0000R\u000e\u0010\f\u001a\u00020\u0004X\u0082D\u00a2\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\u000eX\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001c"}, d2 = {"Lru/netology/service/FCMService;", "Lcom/google/firebase/messaging/FirebaseMessagingService;", "()V", "action", "", "appAuth", "Lru/netology/nmedia/auth/AppAuth;", "getAppAuth", "()Lru/netology/nmedia/auth/AppAuth;", "setAppAuth", "(Lru/netology/nmedia/auth/AppAuth;)V", "channelId", "content", "gson", "Lcom/google/gson/Gson;", "handleContent", "", "message", "Lru/netology/service/PushMessage;", "handleLike", "Lru/netology/service/Like;", "handleNewPost", "Lru/netology/service/NewPost;", "onCreate", "onMessageReceived", "Lcom/google/firebase/messaging/RemoteMessage;", "onNewToken", "token", "app_debug"})
 public final class FCMService extends com.google.firebase.messaging.FirebaseMessagingService {
+    @javax.inject.Inject()
+    public ru.netology.nmedia.auth.AppAuth appAuth;
     private final java.lang.String action = "action";
     private final java.lang.String content = "content";
     private final java.lang.String channelId = "remote";
@@ -11,6 +13,15 @@ public final class FCMService extends com.google.firebase.messaging.FirebaseMess
     
     public FCMService() {
         super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final ru.netology.nmedia.auth.AppAuth getAppAuth() {
+        return null;
+    }
+    
+    public final void setAppAuth(@org.jetbrains.annotations.NotNull()
+    ru.netology.nmedia.auth.AppAuth p0) {
     }
     
     @java.lang.Override()
@@ -22,6 +33,9 @@ public final class FCMService extends com.google.firebase.messaging.FirebaseMess
     com.google.firebase.messaging.RemoteMessage message) {
     }
     
+    private final void handleContent(ru.netology.service.PushMessage message) {
+    }
+    
     @java.lang.Override()
     public void onNewToken(@org.jetbrains.annotations.NotNull()
     java.lang.String token) {
@@ -30,7 +44,7 @@ public final class FCMService extends com.google.firebase.messaging.FirebaseMess
     private final void handleLike(ru.netology.service.Like content) {
     }
     
-    @android.annotation.SuppressLint(value = {"StringFormatMatches"})
-    private final void sentPost(ru.netology.service.Post content) {
+    @android.annotation.SuppressLint(value = {"StringFormatInvalid"})
+    private final void handleNewPost(ru.netology.service.NewPost content) {
     }
 }
--- app/src/main/java/ru/netology/service/FCMService.kt
+++ app/src/main/java/ru/netology/service/FCMService.kt
@@ -11,14 +11,20 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import ru.netology.R
+import ru.netology.nmedia.auth.AppAuth
+import java.lang.IllegalArgumentException
+import javax.inject.Inject
 import kotlin.random.Random
 
+
 class FCMService : FirebaseMessagingService() {
+
+    @Inject
+    lateinit var appAuth: AppAuth
     private val action = "action"
     private val content = "content"
     private val channelId = "remote"
     private val gson = Gson()
-
 
     override fun onCreate() {
         super.onCreate()
@@ -35,76 +41,106 @@ class FCMService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
-
-        message.data[action]?.let {
-            try {
+        try {
+            message.data[action]?.let {
                 when (Action.valueOf(it)) {
-                    Action.LIKE -> handleLike(gson.fromJson(message.data[content], Like::class.java))
-                    Action.POST -> sentPost(gson.fromJson(message.data[content], Post::class.java))
+                    Action.LIKE -> handleLike(
+                        gson.fromJson(
+                            message.data[content],
+                            Like::class.java
+                        )
+                    )
+                    Action.NEW_POST -> handleNewPost(
+                        gson.fromJson(
+                            message.data[content], NewPost::class.java
+                        )
+                    )
                 }
-            } catch (e: IllegalArgumentException) {
-                return@let
+                return
             }
+            handleContent(gson.fromJson(message.data[content], PushMessage::class.java))
+        } catch (e: IllegalArgumentException) {
+            println("No such action class, $e")
+            return
         }
     }
 
+    private fun handleContent(message: PushMessage) {
+        val id = appAuth.authStateFlow.value.id
+        when (message.recipientId) {
+            id, null -> {
+                val notification = NotificationCompat.Builder(this, channelId)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle(message.content)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .build()
+
+                NotificationManagerCompat.from(this)
+                    .notify(Random.nextInt(100_000), notification)
+            }
+            else -> appAuth.sendPushToken()
+        }
+
+    }
+
     override fun onNewToken(token: String) {
-        println(token)
+        appAuth.sendPushToken(token)
     }
 
     private fun handleLike(content: Like) {
         val notification = NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle(
-                        getString(
-                                R.string.notification_user_liked,
-                                content.userName,
-                                content.postAuthor,
-                        )
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_liked,
+                    content.userName,
+                    content.postAuthor,
                 )
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .build()
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
 
         NotificationManagerCompat.from(this)
-                .notify(Random.nextInt(100_000), notification)
+            .notify(Random.nextInt(100_000), notification)
     }
 
-    @SuppressLint("StringFormatMatches")
-    private fun sentPost(content: Post) {
+    @SuppressLint("StringFormatInvalid")
+    private fun handleNewPost(content: NewPost) {
         var notification = NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle(
-                        getString(
-                                R.string.notification_user_post,
-                                content.userName,
-                                content.newPost,
-                        )
-                )
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setStyle(NotificationCompat.BigTextStyle()
-                        .bigText(content.newPost))
-                .build()
-
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(getString(R.string.notification_new_post, content.userName))
+            .setContentText(content.postText)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(content.postText)
+            )
+            .build()
         NotificationManagerCompat.from(this)
-                .notify(Random.nextInt(100_000), notification)
+            .notify(Random.nextInt(100_000), notification)
     }
 }
 
 enum class Action {
     LIKE,
-    POST,
+    NEW_POST,
 }
 
+
 data class Like(
-        val userId: Long,
-        val userName: String,
-        val postId: Long,
-        val postAuthor: String,
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postAuthor: String,
 )
 
-data class Post(
-        val userId: Long,
-        val userName: String,
-        val postId: Long,
-        val newPost: String,
+data class NewPost(
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postText: String,
+)
+
+data class PushMessage(
+    val recipientId: Long?,
+    val content: String
 )
