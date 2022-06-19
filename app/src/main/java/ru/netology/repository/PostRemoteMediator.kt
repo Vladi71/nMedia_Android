69617351d39222bcd48300b23ee110534c72f3b7
Vladi71 <Ronrs6@mail.ru>
2022-06-19 14:25:54 +0300

    Архитектуры, иправил недочеты, и убрал build

--- app/src/main/java/ru/netology/repository/PostRemoteMediator.kt
+++ app/src/main/java/ru/netology/repository/PostRemoteMediator.kt
@@ -68,7 +68,6 @@ class PostRemoteMediator @Inject constructor(
                             )
                         )
                     }
-
                     LoadType.APPEND -> {
                         postRemoteKeyDao.insert(
                             PostRemoteKeyEntity(
